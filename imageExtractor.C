91cbf9cb8dc3ce4242e6d22b047b07ba52b4c6be
Bryan Sanghyuk Kim <bryan.sanghyuk.kim@gmail.com>
2017-01-30 12:09:24 -0500

    Added doxygen documentation

--- imageExtractor.C
+++ imageExtractor.C
@@ -1,3 +1,9 @@
+/**
+ * @file imageExtractor.C
+ *
+ * @brief Source file for imageExtractor.
+ */
+
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
 // imageExtractor.C                                                          //
--- imageExtractor.h
+++ imageExtractor.h
@@ -1,3 +1,9 @@
+/**
+ * @file imageExtractor.h
+ *
+ * Header file for imageExtractor.
+ */
+
 #ifndef __IMAGEEXTRACTOR_H__  
 #define __IMAGEEXTRACTOR_H__  
 
@@ -33,7 +39,8 @@
 #include <properties.hpp>
 
 /**
- *A container for holding all metadata fields and passing into createImage().
+ * @struct metadata
+ * @brief A container for holding all metadata fields and passing into createImage().
  */
 struct metadata
 {
@@ -55,12 +62,14 @@ struct metadata
 };
 
 /**
- * Reads data file in CARE format and generates image in output directory.
+ * @fn int processCAREdata(TFile *file, TPaveText *pt, TH2F *hcamera, TCanvas *ccamera, std::vector<double> v_xcoord, std::vector<double> v_ycoord, int num_channels, std::string output_dir)
+ *
+ * @brief Reads data file in CARE format and generates image in output directory.
  *
  * @param file Tfile object from ROOT data file
  * @param pt A text box containing information on event
  * @param hcamera Histogram containing the summed charge in each channel
- * @param ccanvas TCanvas containing image
+ * @param ccamera TCanvas containing image
  * @param v_xcoord vector of x coordinates for telescope channels
  * @param v_ycoord vector of y coordinates for telescope channels
  * @param num_channels number of channels in telescope data (from config file)
@@ -71,12 +80,14 @@ struct metadata
 int processCAREdata(TFile *file, TPaveText *pt, TH2F *hcamera, TCanvas *ccamera, std::vector<double> v_xcoord, std::vector<double> v_ycoord, int num_channels, std::string output_dir);
 
 /**
- * Reads data file in Event Display format and generates image in output directory.
+ * @fn int processEDdata(TFile *file, TPaveText *pt, TH2F *hcamera, TCanvas *ccamera, std::vector<double> v_xcoord, std::vector<double> v_ycoord, int num_channels, std::string output_dir)
+ *
+ * @brief Reads data file in Event Display format and generates image in output directory.
  *
  * @param file Tfile object from ROOT data file
  * @param pt A text box containing information on event
  * @param hcamera Histogram containing the summed charge in each channel
- * @param ccanvas TCanvas containing image
+ * @param ccamera TCanvas containing image
  * @param v_xcoord vector of x coordinates for telescope channels
  * @param v_ycoord vector of y coordinates for telescope channels
  * @param num_channels number of channels in telescope data (from config file)
@@ -87,7 +98,9 @@ int processCAREdata(TFile *file, TPaveText *pt, TH2F *hcamera, TCanvas *ccamera,
 int processEDdata(TFile *file, TPaveText *pt, TH2F *hcamera, TCanvas *ccamera, std::vector<double> v_xcoord, std::vector<double> v_ycoord, int num_channels, std::string output_dir);
 
 /**
- * Gets starting sample for trace integration by calculating first half-max
+ * @fn int getFirstBin(unsigned short int ***trace, int tel, int channel, int num_samples,int ped_rm)
+ *
+ * @brief Gets starting sample for trace integration by calculating first half-max
  * sample and checking boundary conditions.
  *
  * @param trace 3D matrix containing charge values per sample per channel
@@ -95,14 +108,16 @@ int processEDdata(TFile *file, TPaveText *pt, TH2F *hcamera, TCanvas *ccamera, s
  * @param tel number of current telescope
  * @param channel number of current channel
  * @param num_samples total number of samples per channel
- * @ped_rm pedestal subtraction from charge counts
+ * @param ped_rm pedestal subtraction from charge counts
  *
  * @return returns number of starting sample for trace integration
  */
 int getFirstBin(unsigned short int ***trace, int tel, int channel, int num_samples,int ped_rm);
 
 /**
- * Reads telescope configuration file and saves channel-position mappings
+ * @fn void readConfig(std::string filepath, std::vector<int>& channels, std::vector<double>& x_coord, std::vector<double>& y_coord)
+ *
+ * @brief Reads telescope configuration file and saves channel-position mappings
  * into three vectors.
  *
  * @param filepath Filepath for telescope config file
@@ -114,7 +129,9 @@ int getFirstBin(unsigned short int ***trace, int tel, int channel, int num_sampl
 void readConfig(std::string filepath, std::vector<int>& channels, std::vector<double>& x_coord, std::vector<double>& y_coord);
 
 /**
- * Takes TH2F histogram and metadata container and generates event image.
+ * @fn int createImage(TH2F *hcam, std::string img_name,struct metadata md)
+ *
+ * @brief Takes TH2F histogram and metadata container and generates event image.
  *
  * @param hcam 2D histogram containing the summed charge in each telescope
  * channel
