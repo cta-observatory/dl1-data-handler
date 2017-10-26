d0c82ce7f0ba7e7a49afe9044d1147711521c169
Bryan Kim <bsk2133@columbia.edu>
2017-10-26 01:35:23 -0400

    Script for plotting histograms of number of triggered telescopes
    Former-commit-id: 8372e5ebee3bd1476d11ce7d9095d9653a0b281c [formerly 8372e5ebee3bd1476d11ce7d9095d9653a0b281c [formerly d2b6d1af99965d4f5081b7f23ba9eced9247bd4e]]
    Former-commit-id: 31f213052e2938cfcec9f5d20a493a695a386b5b
    Former-commit-id: 8174eacbe9eaa4e2362a34aa0c4d81116b4d2030

---
+++ ctapipe/v2_pytables/scripts/plot_numtel.py
@@ -0,0 +1,122 @@
+import numpy as np
+from tables import *
+from ctapipe.io.hessio import hessio_event_source
+#from ctapipe.visualization import CameraDisplay
+from ctapipe.instrument import CameraGeometry
+from ctapipe.image.charge_extractors import NeighbourPeakIntegrator
+#from ctapipe.reco import EnergyRegressor
+from ctapipe.calib import pedestals,CameraCalibrator
+from astropy import units as u
+from PIL import Image
+import matplotlib.pyplot as plt
+import pickle as pkl
+
+#telescope constants
+SCT_IMAGE_WIDTH = 120
+SCT_IMAGE_LENGTH = 120
+LST_NUM_PIXELS = 1855
+SCT_NUM_PIXELS = 11328
+SST_NUM_PIXELS = 0 #update
+
+#collect telescope lists
+source_temp = hessio_event_source("gamma_20deg_180deg_run655___cta-prod3-lapalma-2147m-LaPalma-SCT_cone10.simtel.gz",max_events=1)
+
+LST_list = []
+SCT_list = []
+SST_list = []
+
+for event in source_temp: 
+    for i in event.inst.telescope_ids:
+        if event.inst.num_pixels[i] == SCT_NUM_PIXELS:
+            SCT_list.append(i)
+        elif event.inst.num_pixels[i] == LST_NUM_PIXELS:
+            LST_list.append(i)
+        else:
+            SST_list.append(i)
+        #elif event.inst.num_pixels[i] == 
+
+    all_tels = {'SST' : SST_list, 'SCT' : SCT_list, 'LST' : LST_list}
+
+#select telescopes by type
+selected = {}
+TEL_MODE = 'SCT' 
+if TEL_MODE == 'SST':
+    selected['SST'] = SST_list
+elif TEL_MODE == 'SCT':
+    selected['SCT'] = SCT_list
+elif TEL_MODE == 'LST':
+    selected['LST'] = LST_list
+elif TEL_MODE == 'SCT+LST':
+    selected['LST'] = LST_list
+    selected['SCT'] = SCT_list
+elif TEL_MODE == 'SST+SCT':
+    selected['SST'] = SST_list
+    selected['SCT'] = SCT_list
+elif TEL_MODE == 'SST+LST':
+    selected['LST'] = LST_list
+    selected['SST'] = SST_list
+elif TEL_MODE == 'ALL':
+    selected['LST'] = LST_list
+    selected['SCT'] = SCT_list 
+    selected['SST'] = SST_list
+else:
+    raise ValueError('Telescope selection mode not recognized.') 
+
+print("Telescope Mode: ",TEL_MODE)
+
+NUM_TEL = 0
+
+for i in selected.keys():
+    print(i + ": " + str(len(selected[i])) + " out of " + str(len(all_tels[i])) + " telescopes selected.")
+    NUM_TEL += len(selected[i])
+
+source = hessio_event_source("gamma_20deg_180deg_run655___cta-prod3-lapalma-2147m-LaPalma-SCT_cone10.simtel.gz")
+
+trig_nums_list = []
+
+passing_trig_nums_list = []
+
+#bins cuts dict
+
+bins_dict = pkl.load(open("./aux/bins_cuts_dict/bins_cuts_dict_1bin.pkl", "rb" ))
+
+for event in source:
+    num_trig = 0 
+
+    for tel_type in selected.keys():
+        for tel_id in selected[tel_type]:
+            if tel_id in event.r0.tels_with_data:
+                num_trig += 1
+
+    trig_nums_list.append(num_trig)
+    if (event.r0.run_id,event.r0.event_id) in bins_dict:
+        passing_trig_nums_list.append(num_trig)
+
+
+#plot
+
+plt.hist(trig_nums_list, bins=range(NUM_TEL+1))
+plt.xlabel('Number of triggered telescopes')
+plt.ylabel('Number of events')
+plt.axvline(np.mean(trig_nums_list), color='b', linestyle='dashed')
+plt.savefig('all_events_num_trig.png')
+plt.gcf().clear()
+
+plt.hist(passing_trig_nums_list,bins=range(NUM_TEL+1))
+plt.xlabel('Number of triggered telescopes')
+plt.ylabel('Number of events')
+plt.axvline(np.mean(passing_trig_nums_list), color='b', linestyle='dashed')
+plt.savefig('passing_num_trig.png')
+plt.gcf().clear()
+
+plt.hist(trig_nums_list, bins=range(NUM_TEL+1))
+plt.hist(passing_trig_nums_list,bins=range(NUM_TEL+1))
+plt.xlabel('Number of triggered telescopes')
+plt.ylabel('Number of events')
+plt.axvline(np.mean(trig_nums_list), color='b', linestyle='dashed')
+plt.axvline(np.mean(passing_trig_nums_list), color='r', linestyle='dashed')
+plt.savefig('combined_num_trig.png')
+
+print("Mean number of triggered telescopes (all) : {}".format(np.mean(trig_nums_list)))
+print("Mean number of triggered telescopes (pasing) : {}".format(np.mean(passing_trig_nums_list)))
+
