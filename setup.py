57bc2074027680dd76cc211f6d8c6ec6309c15b5
Bryan Kim <bsk2133@columbia.edu>
2017-11-08 15:37:00 -0500

    Added setuptools script
    Former-commit-id: 98c8c3f2bfb85c2ef0a646d62dbbfede2a347e02

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='image_extractor',
+        version='0.2',
+        description='simtel to HDF5 data dumper',
+        url='http://github.com/',
+        author='Bryan Kim',
+        author_email='bsk2133@columbia.edu',
+        license='MIT',
+        packages=['image_extractor''],
+        zip_safe=False)
