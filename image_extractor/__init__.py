2e4f8268ee5e499db56aa33cd39a4045c670b18b
Bryan Kim <bsk2133@columbia.edu>
2017-11-09 08:15:02 -0500

    Refactoring of image_extractor complete. Package setup working.
    Former-commit-id: 190b304cfb6c066dd0cda59b089cf18d932de359

---
+++ image_extractor/__init__.py
@@ -0,0 +1,3 @@
+from . import config
+from . import trace_convertor
+from . import row_types
--- image_extractor/trace_converter.py
+++ image_extractor/trace_converter.py
@@ -1,49 +1,43 @@
+import numpy as np
 
+from image_extractor import ImageExtractor
 
 class TraceConverter:
 
-    NUM_PIXELS = {"SCT":11328}
-    IMAGE_SHAPE = {"SCT":(120,120)}
+    def __init__(self, img_dtype,dim_order,num_channels,scale_factors):
+        self.img_dtype = img_dtype
+        self.dim_order = dim_order
+        self.num_channels = num_channels
+        self.scale_factors = scale_factors
 
-
-
-    def convert_SCT(pixels_vector,peaks_vector,scale_factor,img_dtype,dim_order,channels):
+    def convert_SCT(self,pixels_vector,peaks_vector):
         """
         Converter from ctapipe image pixel vector, peak position vector to numpy array format.
         """ 
         #hardcoded to correct SCT values
         #TODO: find more natural way to handle these?
-        NUM_PIXELS = 11328
-        CAMERA_DIM = (120,120)
+        image_shape = ImageExtractor.IMAGE_SHAPE['SCT']
+        scale_factor = self.scale_factors['SCT']
+        
         ROWS = 15
         MODULE_DIM = (8,8)
         MODULE_SIZE = MODULE_DIM[0]*MODULE_DIM[1]
         MODULES_PER_ROW = [5,9,11,13,13,15,15,15,15,15,13,13,11,9,5]
 
         #counting from the bottom row, left to right
-        MODULE_START_POSITIONS = [(((CAMERA_DIM[0]-MODULES_PER_ROW[j]*MODULE_DIM[0])/2)+(MODULE_DIM[0]*i),j*MODULE_DIM[1]) for j in range(ROWS) for i in range(MODULES_PER_ROW[j])]
+        MODULE_START_POSITIONS = [(((image_shape[0]-MODULES_PER_ROW[j]*MODULE_DIM[0])/2)+(MODULE_DIM[0]*i),j*MODULE_DIM[1]) for j in range(ROWS) for i in range(MODULES_PER_ROW[j])]
 
-        if dim_order == 'channels_first':
-            im_array = np.zeros([channels,CAMERA_DIM[0]*scale_factor,CAMERA_DIM[1]*scale_factor], dtype=img_dtype)
-        elif dim_order == 'channels_last':
-            im_array = np.zeros([CAMERA_DIM[0]*scale_factor,CAMERA_DIM[1]*scale_factor,channels], dtype=img_dtype)
+        if self.dim_order == 'channels_first':
+            im_array = np.zeros([self.num_channels,image_shape[0]*scale_factor,image_shape[1]*scale_factor], dtype=self.img_dtype)
+        elif self.dim_order == 'channels_last':
+            im_array = np.zeros([image_shape[0]*scale_factor,image_shape[1]*scale_factor,self.num_channels], dtype=self.img_dtype)
 
         if pixels_vector is not None:
-            try:
-                assert len(pixels_vector) == NUM_PIXELS
-            except AssertionError as err:
-                logger.exception("Length of pixel vector does not match SCT num pixels.")
-                raise err
             image_exists = True
         else:
             image_exists = False
 
         if peaks_vector is not None:
-            try:
-                assert len(peaks_vector) == NUM_PIXELS
-            except AssertionError as err:
-                logger.exception("Length of peaks vector does not match SCT num pixels.")
-                raise err
             include_timing = True
         else:
             include_timing = False
@@ -58,11 +52,11 @@ class TraceConverter:
                     scaled_region = [(x+i,y+j) for i in range(0,scale_factor) for j in range(0,scale_factor)]
 
                     for (x_coord,y_coord) in scaled_region:
-                        if dim_order == 'channels_first':
+                        if self.dim_order == 'channels_first':
                             im_array[0,x_coord,y_coord] = pixels_vector[pixel_index]
                             if include_timing:
                                 im_array[1,x_coord,y_coord] = peaks_vector[pixel_index]
-                        elif dim_order == 'channels_last':
+                        elif self.dim_order == 'channels_last':
                             im_array[x_coord,y_coord,0] = pixels_vector[pixel_index]
                             if include_timing:
                                 im_array[x_coord,y_coord,1] = peaks_vector[pixel_index]
@@ -71,4 +65,7 @@ class TraceConverter:
 
         return im_array
 
-
+    def convert_LST(self,pixels_vector,peaks_vector):
+        raise NotImplementedError
+    def convert_SST(self,pixels_vector,peaks_vector):
+        raise NotImplementedError
