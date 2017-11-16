7cf235fb28b190ca37aaf021647915d3e9c064b7
Bryan Kim <bsk2133@columbia.edu>
2017-11-16 17:29:46 -0500

    Reorganized config file options
    Former-commit-id: 31610f3cfda7d5668a0b963bd27fe5edc18fc301

--- image_extractor/config.py
+++ image_extractor/config.py
@@ -6,23 +6,24 @@ from configobj import ConfigObj
 from validate import Validator
 
 config_spec = """
-mode = option('gh_class','energy_recon', default='gh_class')
-storage_mode = option('all','mapped', default='all')
-use_pkl_dict = boolean(default=True)
+mode = option('tel_id','tel_type', default='tel_type')
 [image]
-mode = option('PIXELS_3C','PIXELS_1C','PIXELS_TIMING_2C',PIXELS_TIMING_3C',default='PIXELS_3C')
-scale_factor = integer(min=1,default=2)
+mode = option('vector','array',default='array')
+format = option('charge_only','charge_timing','charge_timing_padding',default='charge_only')
+array_scale_factor = integer(min=1,default=2)
 dtype = option('uint32', 'int16', 'int32', 'uint16',default='uint16')
-dim_order = option('channels_first','channels_last',default='channels_last')
+array_dim_order = option('channels_first','channels_last',default='channels_last')
 [telescope]
-type_mode = option('SST', 'SCT', 'LST', 'SST+SCT','SST+LST','SCT+LST','ALL', default='SCT')
+type_list = list(default=list('SCT'))
 [energy_bins]
+apply_energy_bins = boolean(default=False)
 units = option('eV','MeV','GeV','TeV',default='TeV')
 scale = option('linear','log10',default='log10')
 min = float(default=-1.0)
 max = float(default=1.0)
 bin_size = float(default=0.5)
 [preselection_cuts]
+mode = option('pkl_dict','ctapipe','none',default='pkl_dict')
 MSCW = tuple(default=list(-2.0,2.0))
 MSCL = tuple(default=list(-2.0,5.0))
 EChi2S = tuple(default=list(0.0,None))
@@ -31,7 +32,8 @@ EmissionHeight = tuple(default=list(0.0,50.0))
 MC_Offset = mixed_list(string,string,float,float,default=list('MCxoff','MCyoff',0.0,3.0))
 NImages = tuple(default=list(3,None))
 dES = tuple(default=list(0.0,None))
-[energy_recon]
+[energy_reconstruction]
+    include_label = boolean(default=False)
     gamma_only = boolean(default=True)
     [[bins]]
     units = option('eV','MeV','GeV','TeV',default='TeV')
@@ -39,6 +41,14 @@ dES = tuple(default=list(0.0,None))
     min = float(default=-2.0)
     max = float(default=2.0)
     bin_size = float(default=0.05)
+[preprocessing]
+shuffle = boolean(default=True)
+    [[split]]
+    apply_split = boolean(default=True)
+    train = float(default=0.9)
+    val = float(default=0.1)
+    test = float(default=0.1)
+
 """
 
 logger = logging.getLogger(__name__)
@@ -62,11 +72,12 @@ if __name__ == '__main__':
         spec = s.split('\n')
         config = ConfigObj(args.file, configspec=spec)
         validator = Validator()
-        assert config.validate(validator, copy=True), "Error"
+        assert config.validate(validator, copy=True), "Validation failed on file: {}".format(args.file)
 
     elif args.mode == 'spec':
         with open(args.file, 'wb') as f:
             write(s)
 
     else:
-        print("Invalid mode")
+        logger.error('Invalid command line mode: {}. Valid options: config, val, spec.'.format(args.mode))
+        raise ValueError('Invalid command line mode: {}.'.format(args.mode))
