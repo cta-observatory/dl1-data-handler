258fa6dbde561dbb074df06b23727c921b5ecd09
Bryan Kim <bsk2133@columbia.edu>
2017-12-04 20:58:57 -0500

    Reorganized convert functions into a single convert function and a dict of injunction tables

--- image_extractor/image.py
+++ image_extractor/image.py
@@ -1,6 +1,13 @@
+import logging
+
 import numpy as np
 
+import image_extractor
+
+TEL_NUM_PIXELS = {'LST':1855,'MSTF':1855, 'MSTN':1764,'MSTS':11328,'SST1':1296, 'SSTA':2368, 'SSTC':2048}
 IMAGE_SHAPES = {'MSTS': (120, 120)}
+
+logger = logging.getLogger(__name__)
 
 
 class TraceConverter:
@@ -11,15 +18,71 @@ class TraceConverter:
         self.num_channels = num_channels
         self.scale_factors = scale_factors
 
-    def convert_MSTS(self, pixels_vector, peaks_vector):
+        #create injunction tables for each telescope type
+        self.injunction_tables = {}
+        self.injunction_tables['MSTS'] = __generate_table_MSTS()
+
+    def convert(self,pixels_vector,peaks_vector,tel_type):
         """
         Converter from ctapipe image pixel vector,
         peak position vector to numpy array format.
         """
         
-        image_shape = IMAGE_SHAPES['MSTS']
-        scale_factor = self.scale_factors['MSTS']
+        image_shape = IMAGE_SHAPES[tel_type]
+        scale_factor = self.scale_factors[tel_type]
+        injunction_table = self.injunction_tables[tel_type]
 
+        if self.dim_order == 'channels_first':
+            shape = [self.num_channels,
+                    image_shape[0] * scale_factor,
+                    image_shape[1] * scale_factor]
+        elif self.dim_order == 'channels_last':
+            shape = [image_shape[0] * scale_factor,
+                    image_shape[1] * scale_factor,
+                    self.num_channels]
+    
+        if pixels_vector is None:
+            return np.zeros(shape,dtype=self.img_dtype)
+        else:
+            
+            img_array = np.empty(shape,dtype=self.img_dtype)
+
+            #preprocess image vector - truncate at 0, scale by 100, round to integer
+            pixels_vector = np.around(np.multiply(pixels_vector.clip(min=0),100))
+
+            if len(pixels_vector) != TEL_NUM_PIXELS[tel_type]:
+                raise ValueError("Size of image vector does not match telescope type: {} {}".format(len(pixels_vector),TEL_NUM_PIXELS[tel_type]))
+
+            if peaks_vector is not None:
+                if len(pixels_vector) != len(peaks_vector):
+                    raise ValueError("Size of image vector and peak time vector do not match: {} {}".format(len(pixels_vector),len(peaks_vector)))
+                if self.img_channels < 2:
+                    raise ValueError('To include timing information, num channels must be >= 2.')
+
+                for i, (charge, peak) in enumerate(zip(pixels_vector,peaks_vector)):
+                    x,y = injunction_table[i]
+                    if self.dim_order == 'channels_first':
+                        img_array[0,x,y] = charge
+                        img_array[1,x,y] = peak
+                    elif self.dim_order == 'channels_last':
+                        img_array[x,y,0] = charge
+                        img_array[x,y,1] = peak
+            else:
+                for i,charge in enumerate(pixels_vector):
+                    x,y = injunction_table[i]
+                    if self.dim_order == 'channels_first':
+                        img_array[0,x,y] = charge
+                    elif self.dim_order == 'channels_last':
+                        img_array[x,y,0] = charge
+ 
+            return img_array
+
+    @staticmethod
+    def __generate_table_MSTS():
+        """
+        Function returning MSTS injunction table
+        """
+        
         ROWS = 15
         MODULE_DIM = (8, 8)
         MODULE_SIZE = MODULE_DIM[0] * MODULE_DIM[1]
@@ -41,75 +104,16 @@ class TraceConverter:
             5]
 
         # counting from the bottom row, left to right
-        MODULE_START_POSITIONS = [(((image_shape[0] - MODULES_PER_ROW[j] *
+        MODULE_START_POSITIONS = [(((IMAGE_SHAPES['MSTS'][0] - MODULES_PER_ROW[j] *
                                      MODULE_DIM[0]) / 2) +
                                    (MODULE_DIM[0] * i), j * MODULE_DIM[1])
                                   for j in range(ROWS)
                                   for i in range(MODULES_PER_ROW[j])]
 
-        if self.dim_order == 'channels_first':
-            shape = [self.num_channels,
-                    image_shape[0] * scale_factor,
-                    image_shape[1] * scale_factor]
-        elif self.dim_order == 'channels_last':
-            shape = [image_shape[0] * scale_factor,
-                    image_shape[1] * scale_factor,
-                    self.num_channels]
-    
-        img_array = np.zeros(shape,dtype=self.img_dtype)
+        injunction_table = [(int(x_0 + int(i / MODULE_DIM[0])),y_0 + i % MODULE_DIM[1]) 
+                            for (x_0,y_0) in MODULE_START_POSITIONS
+                            for i in range(MODULE_SIZE)]
 
-        image_exists = True if pixels_vector is not None else False
-        include_timing = True if peaks_vector is not None else False
+        return injunction_table
 
-        if include_timing and self.img_channels < 2:
-            raise ValueError('To include timing information, num channels must be >=2.')
-
-        #preprocess pixels vector
-        # truncate at 0, scale by 100
-        pixels_vector = pixels_vector.clip(min=0)
-        pixels_vector = np.around(np.multiply(pixels_vector,100))
-
-        if image_exists:
-            pixel_index = 0
-            for (x_start, y_start) in MODULE_START_POSITIONS:
-                for i in range(MODULE_SIZE):
-                    x = (int(x_start + int(i / MODULE_DIM[0]))) * scale_factor
-                    y = (y_start + i % MODULE_DIM[1]) * scale_factor
-
-                    scaled_region = [(x+i, y+j) for i in range(0, scale_factor)
-                                     for j in range(0, scale_factor)]
-
-                    for (x_coord, y_coord) in scaled_region:
-                        if self.dim_order == 'channels_first':
-                            charge_pos = (0,x_coord,y_coord)
-                            timing_pos = (1,x_coord,y_coord)
-                        elif self.dim_order == 'channels_last':
-                            charge_pos = (x_coord,y_coord,0)
-                            timing_pos = (x_coord,y_coord,1)
-                        
-                        img_array[charge_pos] = pixels_vector[pixel_index]
-                        if include_timing:
-                            peaks_vector = peaks_vector.astype
-                            img_array[timing_pos] = peaks_vector[pixel_index]
-                pixel_index += 1
-
-        return img_array
-
-    def convert_LST(self, pixels_vector, peaks_vector):
-        raise NotImplementedError
-
-    def convert_MSTF(self, pixels_vector, peaks_vector):
-        raise NotImplementedError
-
-    def convert_MSTN(self, pixels_vector, peaks_vector):
-        raise NotImplementedError
-
-    def convert_SST1(self, pixels_vector, peaks_vector):
-        raise NotImplementedError
-
-    def convert_SSTA(self, pixels_vector, peaks_vector):
-        raise NotImplementedError
-
-    def convert_SSTC(self, pixels_vector, peaks_vector):
-        raise NotImplementedError
 
