8d149015492c8b56c1843738feb6cc25dd21c40f
Bryan Kim <bsk2133@columbia.edu>
2017-12-06 21:33:45 -0500

    Updated setup.py to version 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='image_extractor',
-      version='0.2',
+      version='0.4.0',
       description='simtel to HDF5 data dumper',
       url='http://github.com/',
       license='MIT',
