03930c2b20979a889a7be742156ab2c2b055e55c
Bryan Kim <bsk2133@columbia.edu>
2018-01-29 18:45:29 -0500

    Updated setup and travis files to match package name change from image_extractor to image-extractor

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='image_extractor',
+setup(name='image-extractor',
       version='0.5.1',
       description='simtel to HDF5 data dumper',
       url='http://github.com/',
       license='MIT',
-      packages=['image_extractor'],
+      packages=['image-extractor'],
       dependencies=[],
       dependency_links=['git+http://github.com/cta-observatory/ctapipe'],
       zip_safe=False)
