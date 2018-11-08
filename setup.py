9279a4018f6753445899e6e2d81d676f1ae3f361
bryankim96 <bryan.sanghyuk.kim@gmail.com>
2018-11-08 12:11:07 +0100

    Update package names in setup/installation files.
    Former-commit-id: 521c3b5d445c5426be4cad6662c9417e15123db1 [formerly 521c3b5d445c5426be4cad6662c9417e15123db1 [formerly b57d63ded73f7b0095709e97a3359902530628a9]]
    Former-commit-id: 6a648cb9fe9a35b45616bbe9f9a47cdbf9054eaa
    Former-commit-id: 75356df690804a44ea17405b20cda4ceea0422ba

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='image-extractor',
-      version='0.5.1',
-      description='simtel to HDF5 data dumper',
-      url='http://github.com/',
+setup(name='dl1-data-handler',
+      version='0.6.0',
+      description='dl1 HDF5 data writer + reader + processor',
+      url='http://github.com/cta-observatory/dl1-data-handler',
       license='MIT',
-      packages=['image-extractor'],
+      packages=['dl1-data-handler'],
       dependencies=[],
       dependency_links=['git+http://github.com/cta-observatory/ctapipe'],
-      zip_safe=False)
+      zip_safe=True)
