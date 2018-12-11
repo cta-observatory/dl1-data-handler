8e2f75baa197ca253430f333efd35ce973af24c9
bryankim96 <bryan.sanghyuk.kim@gmail.com>
2018-12-11 00:48:45 -0800

    Moved optics and camera fields from array info table to tel type table.
    Removed unnecessary import of Float64Col.
    Updated docstrings and comments.
    Formatted to PEP8.

--- dl1_data_handler/table_definitions.py
+++ dl1_data_handler/table_definitions.py
@@ -1,4 +1,4 @@
-"""Module for custom PyTables row classes.
+"""Describe table row formats for CTAMLDataDumper output files.
 
 These row classes below define the structure of the tables in
 the output PyTables .h5 files.
@@ -6,12 +6,11 @@ the output PyTables .h5 files.
 """
 
 from tables import (IsDescription, UInt32Col, UInt16Col, UInt8Col,
-                    Float64Col, Float32Col, StringCol)
+                    Float32Col, StringCol)
 
 
 class EventTableRow(IsDescription):
-
-    """Row descriptor class for event table.
+    """Describe row format for event table.
 
     Contains event-level information, mostly from Monte Carlo simulation
     parameters. NOTE: Additional columns are added dynamically to some tables,
@@ -19,25 +18,33 @@ class EventTableRow(IsDescription):
 
     Attributes
     ----------
-    event_id : UInt32Col
+    event_id : tables.UInt32Col
         Shower event id.
-    obs_id : UInt32Col
-        Shower observation (run) id. Replaces "run_id"
-    shower_primary_id : UInt8Col
-        Particle type id for the shower primary particle. From Monte Carlo simulation parameters.
-    core_x : Float64Col
-        Shower core position x coordinate. From Monte Carlo simulation parameters.
-    core_y : Float64Col
-        Shower core position y coordinate. From Monte Carlo simulation parameters.
-    h_first_int : Float64Col
-        Height of shower primary particle first interaction. From Monte Carlo simulation parameters.
-    mc_energy : Float64Col
-        Energy of the shower primary particle. From Monte Carlo simulation parameters.
-    az : Float32Col
+    obs_id : tables.UInt32Col
+        Shower observation (run) id. Replaces old "run_id" in ctapipe r0
+        container.
+    shower_primary_id : tables.UInt8Col
+        Particle type id for the shower primary particle. From Monte Carlo
+        simulation parameters.
+    core_x : tables.Float32Col
+        Shower core position x coordinate. From Monte Carlo simulation
+        parameters.
+    core_y : tables.Float32Col
+        Shower core position y coordinate. From Monte Carlo simulation
+        parameters.
+    h_first_int : tables.Float32Col
+        Height of shower primary particle first interaction. From Monte Carlo
+        simulation parameters.
+    mc_energy : tables.Float32Col
+        Energy of the shower primary particle. From Monte Carlo simulation
+        parameters.
+    az : tables.Float32Col
         Shower azimuth angle. From Monte Carlo simulation parameters.
-    alt : Float32Col
+    alt : tables.Float32Col
         Shower altitude (zenith) angle. From Monte Carlo simulation parameters.
+
     """
+
     event_id = UInt32Col()
     obs_id = UInt32Col()
     shower_primary_id = UInt8Col()
@@ -51,7 +58,7 @@ class EventTableRow(IsDescription):
 
 
 class TelTableRow(IsDescription):
-    """Row descriptor class for telescope type table.
+    """Describe row format for telescope type table.
 
     Contains parameter information for each telescope type in the data.  NOTE:
     Additional columns are added dynamically to some tables,
@@ -59,44 +66,48 @@ class TelTableRow(IsDescription):
 
     Attributes
     ----------
-    type : StringCol
+    type : tables.StringCol
         Telescope type name (i.e. 'LST:LSTCam')
-    num_pixels: UInt32Col
+    optics : tables.StringCol
+        Telescope optics type name (i.e. 'LST').
+    camera : tables.StringCol
+        Telescope camera type name (i.e. 'LSTCam').
+    num_pixels: tables.UInt32Col
         Number of pixels in the telescope camera.
+
     """
 
     type = StringCol(20)
+    optics = StringCol(20)
+    camera = StringCol(20)
     num_pixels = UInt32Col()
 
-class ArrayTableRow(IsDescription):
-    """Row descriptor class for telescope array table.
 
-    Contains parameter information for each telescope
-    in the array. NOTE: Additional columns are added dynamically to some tables,
-    see the github wiki page for the full table/data format descriptions.
+class ArrayTableRow(IsDescription):
+    """Describe row format for telescope array table.
+
+    Contains parameter information for each telescope in the array.
+    NOTE: Additional columns are added dynamically to some tables, see the
+    github wiki page for the full table/data format descriptions.
 
     Attributes
     ----------
-    id : UInt8Col
+    id : tables.UInt8Col
         Telescope id (unique).
-    x : Float32Col
-        Telescope position x coordinate relative to the center of the array.
-    y : Float32Col
-        Telescope position y coordinate relative to the center of the array.
-    z : Float32Col
-        Telescope position z coordinate (height) relative to the CORSIKA observatory altitude.
-    type : StringCol
+    type : tables.StringCol
         Telescope type name (i.e. 'LST:LSTCam').
-    camera : StringCol
-        Telescope camera type name (i.e. 'LSTCam').
-    optics : StringCol
-        Telescope optics type name (i.e. 'LST').
+    x : tables.Float32Col
+        Telescope position x coordinate relative to the center of the array.
+    y : tables.Float32Col
+        Telescope position y coordinate relative to the center of the array.
+    z : tables.Float32Col
+        Telescope position z coordinate (height) relative to the CORSIKA
+        observatory altitude.
+
     """
 
     id = UInt16Col()
+    type = StringCol(20)
     x = Float32Col()
     y = Float32Col()
     z = Float32Col()
-    type = StringCol(20)
-    camera = StringCol(20)
-    optics = StringCol(20)
