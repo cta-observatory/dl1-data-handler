9376b5fc83c7206537b4f10ca17417f7cbe8c134
Bryan Kim <bryan.sanghyuk.kim@gmail.com>
2019-02-14 20:33:46 -0700

    Fixed dependencies.

--- setup.py
+++ setup.py
@@ -7,8 +7,12 @@ setup(name='dl1_data_handler',
       url='http://github.com/cta-observatory/dl1-data-handler',
       license='MIT',
       packages=['dl1_data_handler'],
-      dependencies=[
-         numpy>=1.15.0,
-         tables>=3.4.4],
-      dependency_links=['https://github.com/cta-observatory/ctapipe/tarball/v0.6.2#egg=ctapipe-0.6.2'],
+      install_requires=[
+          'numpy>=1.15.0', 
+          'tables>=3.4.4',
+          'ctapipe @ https://api.github.com/repos/cta-observatory/ctapipe/tarball/0.6.2',
+          'ctapipe-extra @ https://api.github.com/repos/cta-observatory/ctapipe-extra/tarball/0.2.16'
+          ],
+      dependency_links=['git+https://github.com/cta-observatory/ctapipe.git#egg=ctapipe-0.6.2',
+          'git+https://github.com/cta-observatory/ctapipe-extra.git#egg=ctapipe-extra-0.2.16'],
       zip_safe=True)
