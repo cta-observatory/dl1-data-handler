a6e021599bd254b920e6ee5f83abad6c401f1815
Bryan Kim <bryan.sanghyuk.kim@gmail.com>
2019-02-19 07:12:35 +0100

    Fixed installation issues.
    Former-commit-id: 1a7f6851f897726793221a380547065af498434c

--- setup.py
+++ setup.py
@@ -10,9 +10,8 @@ setup(name='dl1_data_handler',
       install_requires=[
           'numpy>=1.15.0', 
           'tables>=3.4.4',
-          'ctapipe @ https://api.github.com/repos/cta-observatory/ctapipe/tarball/0.6.2',
-          'ctapipe-extra @ https://api.github.com/repos/cta-observatory/ctapipe-extra/tarball/0.2.16'
-          ],
-      dependency_links=['git+https://github.com/cta-observatory/ctapipe.git#egg=ctapipe-0.6.2',
-          'git+https://github.com/cta-observatory/ctapipe-extra.git#egg=ctapipe-extra-0.2.16'],
+          'pyhessio @ https://api.github.com/repos/cta-observatory/pyhessio/tarball/v2.1.1',
+          'ctapipe @ https://api.github.com/repos/cta-observatory/ctapipe/tarball/v0.6.2',
+          'ctapipe-extra @ https://api.github.com/repos/cta-observatory/ctapipe-extra/tarball/v0.2.16'],
+      dependency_links=[],
       zip_safe=True)
