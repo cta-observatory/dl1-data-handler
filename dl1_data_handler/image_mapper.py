a3a872372a6b369b32d3c8e03fa9c9c893fa385f
Ari Brill <aryeh.brill@gmail.com>
2019-03-01 17:23:28 -0500

    Fix bugs with loading data
    Former-commit-id: 283f38fb69abd19d5dfc8b017ae24be0d22c3452

--- dl1_data_handler/image_mapper.py
+++ dl1_data_handler/image_mapper.py
@@ -1,5 +1,4 @@
-def get_camera_type(tel_type):
-    return tel_type.split(':')[1]
+import numpy as np
 
 class ImageMapper():
     
@@ -8,14 +7,12 @@ class ImageMapper():
         self.channels = channels
         self.mapping_tables = {}
         self.image_shape = {
-                'LST_LSTCam': (108, 108, 1),
-                'MST_FlashCam': (110, 110, 1),
-                'SST1M_DigiCam': (94, 94, 1),
+                'LST_LSTCam': (110, 110, 1),
+                'MST_FlashCam': (112, 112, 1),
+                'MST_NectarCam': (110, 110, 1),
+                'SST1M_DigiCam': (96, 96, 1),
                 }
    
     """Returns: image (WHC np.float32 array)"""
     def map_image(self, vector, tel_type):
-        camera_type = get_camera_type(tel_type)
-        raise NotImplementedError
-
-
+        return np.resize(vector, self.image_shape[tel_type])
