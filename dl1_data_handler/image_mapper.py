020f6623231eb43ca96fe85e5861aadbd2628bfa
Ari Brill <aryeh.brill@gmail.com>
2019-03-14 10:52:47 -0400

    Respond to comments and improve style
    Former-commit-id: 066bc56726105c19203fc2ddbc5f6467133f4247

--- dl1_data_handler/image_mapper.py
+++ dl1_data_handler/image_mapper.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 class ImageMapper():
-    
+
     def __init__(self, mapping_method, channels):#", settings=defaults, ..."):
         self.mapping_method = mapping_method
         self.channels = channels
         self.mapping_tables = {}
         self.image_shape = {
-                'LST_LSTCam': (110, 110, 1),
-                'MST_FlashCam': (112, 112, 1),
-                'MST_NectarCam': (110, 110, 1),
-                'SST1M_DigiCam': (96, 96, 1),
-                }
-   
-    """Returns: image (WHC np.float32 array)"""
+            'LST_LSTCam': (110, 110, 1),
+            'MST_FlashCam': (112, 112, 1),
+            'MST_NectarCam': (110, 110, 1),
+            'SST1M_DigiCam': (96, 96, 1),
+            }
+
+    # TODO: Replace this placeholder method with valid image mapping methods
     def map_image(self, vector, tel_type):
+        # Fill an array of the right size with copies of the vector
         return np.resize(vector, self.image_shape[tel_type])
