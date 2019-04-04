Fixed issue with mapping table rotation.
Temporarily removed FACT mapping table due to shape mismatch issue.

Finished data reader part of demo notebook.


Former-commit-id: ca269286ccadb59d3c06fe5206c255b070a57063
Former-commit-id: ccaed83cbd6310c51f802f022f5f4a46f8a263de
Former-commit-id: 4c420dc5d50ec71be6fb415c670ba6ad29a931fc

dl1_data_handler/image_mapper.py
@@ -7,7 +7,6 @@ from scipy.sparse import csr_matrix
 from collections import Counter
 
 from ctapipe.instrument.camera import CameraGeometry
-from astropy import units as u
 
 from dl1_data_handler import processor
 
@@ -80,8 +79,8 @@ class ImageMapper:
             self.camera_geometries[camtype] = CameraGeometry.from_name(camtype)
 
             # Rotate it if necessary
-            if camtype in ['LSTCam', 'NectarCam', 'MAGICCam']:
-                self.camera_geometries[camtype].rotate(90.0 * u.deg - self.camera_geometries[camtype].pix_rotation)
+            #if camtype in ['LSTCam', 'NectarCam', 'MAGICCam']:
+                #self.camera_geometries[camtype].rotate(90.0 * u.deg - self.camera_geometries[camtype].pix_rotation)
 
             map_method = self.mapping_method[camtype]
             if map_method not in ['oversampling', 'rebinning', 'nearest_interpolation', 'bilinear_interpolation',
@@ -123,7 +122,8 @@ class ImageMapper:
                 channels  # number of channels
                 )
 
-            self.mapping_tables[camtype] = self.generate_table(camtype)
+            if camtype != "FACT":
+                self.mapping_tables[camtype] = self.generate_table(camtype)
 
     def map_image(self, pixels, camera_type):
         """
@@ -155,7 +155,13 @@ class ImageMapper:
         num_pixels = len(CameraGeometry.from_name(camera_type).pix_id)
         # Get telescope pixel positions and padding for the given tel type
         camgeom = self.camera_geometries[camera_type]
+
         pos = np.column_stack([camgeom.pix_x.value, camgeom.pix_y.value]).T
+
+        if camera_type in ['LSTCam', 'NectarCam', 'MAGICCam']:
+            pixel_rot = 90.0 - camgeom.pix_rotation.value
+            pos = self.rotate_pixel_pos(pos, pixel_rot)
+
         pad = self.padding[camera_type]
         default_pad = self.default_pad
         map_method = self.mapping_method[camera_type]
@@ -881,5 +887,13 @@ class ImageMapper:
         for i in range(1, mapping_matrix3d.shape[0]):
             mapping_matrix3d[i] *= mask
         return mapping_matrix3d
+
+    @staticmethod
+    def rotate_pixel_pos(pos, angle_deg):
+        angle = angle_deg * np.pi/180.0
+        rotation_matrix = np.matrix([[np.cos(angle), -np.sin(angle)],
+                                     [np.sin(angle), np.cos(angle)]], dtype=float)
+        pos_rotated = np.squeeze(np.asarray(np.dot(rotation_matrix, pos)))
+        return pos_rotated
 
 

dl1_data_handler/reader.py
@@ -8,6 +8,9 @@ import tables
 from dl1_data_handler.image_mapper import ImageMapper
 from dl1_data_handler.processor import DL1DataProcessor
 
+def get_camera_type(tel_type):
+    return tel_type.split('_')[1]
+
 class DL1DataReader:
 
     @staticmethod
@@ -186,7 +189,7 @@ class DL1DataReader:
                     'name': 'image',
                     'tel_type': self.tel_type,
                     'base_name': 'image',
-                    'shape': self.image_mapper.image_shape[self.tel_type],
+                    'shape': self.image_mapper.image_shapes[get_camera_type(self.tel_type)],
                     'dtype': np.dtype(np.float32)
                     }
                 ]
@@ -209,7 +212,7 @@ class DL1DataReader:
                     'tel_type': self.tel_type,
                     'base_name': 'image',
                     'shape': ((num_tels,)
-                              + self.image_mapper.image_shape[self.tel_type]),
+                              + self.image_mapper.image_shapes[get_camera_type(self.tel_type)]),
                     'dtype': np.dtype(np.float32)
                     },
                 {
@@ -241,7 +244,7 @@ class DL1DataReader:
                         'tel_type': tel_type,
                         'base_name': 'image',
                         'shape': ((num_tels,)
-                                  + self.image_mapper.image_shape[tel_type]),
+                                  + self.image_mapper.image_shapes[get_camera_type(tel_type)]),
                         'dtype': np.dtype(np.float32)
                         },
                     {
@@ -319,7 +322,7 @@ class DL1DataReader:
         # image of all zeros with be loaded
         for i, channel in enumerate(self.image_channels):
             vector[1:, i] = record[channel]
-        image = self.image_mapper.map_image(vector, tel_type)
+        image = self.image_mapper.map_image(vector, get_camera_type(tel_type))
 
         return image
 