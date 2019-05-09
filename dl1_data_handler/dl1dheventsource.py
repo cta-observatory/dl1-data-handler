c7038afb1c0f3709135886ffe84b30aec46fb179
vuillaut <thomas.vuillaume@gmail.com>
2019-05-09 10:13:47 +0200

    cleaning here and there and move file to self

--- dl1_data_handler/dl1dheventsource.py
+++ dl1_data_handler/dl1dheventsource.py
@@ -36,11 +36,6 @@ class DL1DHEventSource(EventSource):
 
         self.tables = tables
 
-        # if DL1DHEventSource._count > 0:
-        #     self.log.warning("Only one DL1DH event_source allowed at a time. "
-        #                      "Previous DL1DH file will be closed.")
-        #     # self.tables.close()
-        # DL1DHEventSource._count += 1
 
         self.metadata['is_simulation'] = True
 
@@ -54,32 +49,33 @@ class DL1DHEventSource(EventSource):
         self.file.close()
 
     def _generator(self):
-        with self.tables.open_file(self.input_url) as file:
+        with self.tables.open_file(self.input_url) as self.file:
             # the container is initialized once, and data is replaced within
             # it after each yield
             counter = 0
-            eventstream = file.root.Events
+            eventstream = self.file.root.Events
             data = DataContainer()
             data.meta['origin'] = "dl1_data_handler"
 
             data.meta['input_url'] = self.input_url
             data.meta['max_events'] = self.max_events
 
-            tel_types = set(file.root.Array_Information[:]['type'])
-            run_array_direction = file.root._v_attrs['run_array_direction']
+            tel_types = set(self.file.root.Array_Information[:]['type'])
+            run_array_direction = self.file.root._v_attrs['run_array_direction']
             # array_alt_pointing = run_array_direction[1] * u.rad
             # array_az_pointing = run_array_direction[0] * u.rad
 
             tel_ids = {}
             for tel_type in tel_types:
-                tel_ids[tel_type] = file.root.Array_Information[file.root.Array_Information[:]['type'] == tel_type]['id']
+                tel_ids[tel_type] = self.file.root.Array_Information \
+                    [self.file.root.Array_Information[:]['type'] == tel_type]['id']
 
             for event in eventstream:
 
                 if counter == 0:
                     # subarray info is only available when an event is loaded,
                     # so load it on the first event.
-                    data.inst.subarray = self._build_subarray_info(file)
+                    data.inst.subarray = self._build_subarray_info()
 
                 obs_id = event['obs_id']
                 event_id = event['event_id']
@@ -106,10 +102,10 @@ class DL1DHEventSource(EventSource):
                     data.r1.tels_with_data = selected
                     data.dl0.tels_with_data = selected
 
-                # data.trig.tels_with_trigger = (file.
+                # data.trig.tels_with_trigger = (self.file.
                 #                                get_central_event_teltrg_list()) # info not kept
 
-                # time_s, time_ns = file.get_central_event_gps_time()
+                # time_s, time_ns = self.file.get_central_event_gps_time()
                 # data.trig.gps_time = Time(time_s * u.s, time_ns * u.ns,
                 #                           format='unix', scale='utc')
                 data.mc.energy = event['mc_energy'] * u.TeV
@@ -123,7 +119,7 @@ class DL1DHEventSource(EventSource):
                 data.mc.shower_primary_id = event['shower_primary_id']
 
                 # mc run header data
-                self._build_mcheader(file, data)
+                self._build_mcheader(data)
 
                 # this should be done in a nicer way to not re-allocate the
                 # data each time (right now it's just deleted and garbage
@@ -139,8 +135,8 @@ class DL1DHEventSource(EventSource):
                     idxs = event[tel_type.decode() + '_indices']
                     for idx in idxs[idxs > 0]:
                         tel_id = tel_ids[tel_type][np.where(idxs == idx)[0][0]]
-                        charge = file.root[tel_type.decode()][idx]['charge']
-                        peakpos = file.root[tel_type.decode()][idx]['peakpos']
+                        charge = self.file.root[tel_type.decode()][idx]['charge']
+                        peakpos = self.file.root[tel_type.decode()][idx]['peakpos']
 
                         data.dl1.tel[tel_id].image = charge
                         data.dl1.tel[tel_id].pulse_time = peakpos
@@ -150,7 +146,7 @@ class DL1DHEventSource(EventSource):
 
         return
 
-    def _build_subarray_info(self, file):
+    def _build_subarray_info(self):
         """
         constructs a SubarrayDescription object from the info in an DL1 file
 
@@ -166,18 +162,20 @@ class DL1DHEventSource(EventSource):
 
         subarray = SubarrayDescription("MonteCarloArray")
 
-        for tel in file.root.Array_Information:
+        for tel in self.file.root.Array_Information:
             tel_id = tel['id']
             tel_type = tel['type']
-            tel_info = file.root.Telescope_Type_Information[file.root.Telescope_Type_Information[:]['type'] == tel_type][0]
-            subarray.tels[tel_id] = self._build_telescope_description(tel_info)
+            subarray.tels[tel_id] = self._build_telescope_description(tel_type)
             tel_pos = u.Quantity([tel['x'], tel['y'], tel['z']], u.m)
             subarray.positions[tel_id] = tel_pos
 
         return subarray
 
 
-    def _build_telescope_description(self, tel_info):
+    def _build_telescope_description(self, tel_type):
+
+        tel_info = self.file.root.Telescope_Type_Information \
+            [self.file.root.Telescope_Type_Information[:]['type'] == tel_type][0]
 
         camera_name = tel_info['camera'].decode()
         optics_name = tel_info['optics'].decode()
@@ -194,7 +192,7 @@ class DL1DHEventSource(EventSource):
         return TelescopeDescription.from_name(optics_name, camera_name)
 
 
-    def _build_mcheader(self, file, data):
+    def _build_mcheader(self, data):
         """
         Read the mcheader data from the DL1 file and update the data container
 
@@ -206,7 +204,7 @@ class DL1DHEventSource(EventSource):
 
         for k in data.mcheader.keys():
             try:
-                data.mcheader[k] = file.root._v_attrs[k]
+                data.mcheader[k] = self.file.root._v_attrs[k]
             except:
                 warnings.warn(f"item {k} does not exist in the file attributes")
 
@@ -214,7 +212,6 @@ class DL1DHEventSource(EventSource):
 
 '''
 ### TODO:
-- deal with close file (self.file.close() ?)
 - make a version working with ctapipe master (pulse_time)
 - unit test or check that a converted simtel file give the same info in ctapipe containers
 '''
