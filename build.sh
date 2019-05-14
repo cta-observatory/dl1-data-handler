14d7a920f1a004b5faf40bb4c9f1b8045cb45618
Bryan Kim <bryan.sanghyuk.kim@gmail.com>
2019-05-14 15:15:46 -0700

    Updated conda build files.

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-conda config --add channels conda-forge
-conda config --add channels anaconda
-conda config --add channels cta-observatory
+#!/bin/bash
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
