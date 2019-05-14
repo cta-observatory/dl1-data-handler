41a294f73034d11ac03a2b3903732bba6ff4b272
Bryan Kim <bryan.sanghyuk.kim@gmail.com>
2019-05-14 12:38:29 -0700

    Prep for v0.7.5 for conda build and packaging tests.

--- setup.py
+++ setup.py
@@ -10,9 +10,8 @@ setup(name='dl1_data_handler',
       install_requires=[
           'numpy>=1.15.0',
           'tables>=3.4.4',
-          'pyhessio @ https://api.github.com/repos/cta-observatory/pyhessio/tarball/v2.1.1',
-          'ctapipe @ https://api.github.com/repos/cta-observatory/ctapipe/tarball/v0.6.2',
-          'ctapipe-extra @ https://api.github.com/repos/cta-observatory/ctapipe-extra/tarball/v0.2.16',
+          'ctapipe>=0.6.2',
+          'ctapipe-extra >=0.2.17',
           'pytest-cov'],
       dependency_links=[],
       zip_safe=True)
