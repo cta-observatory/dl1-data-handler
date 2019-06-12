4dde2d5f667dbbf53f98df45e550ffaa5bf00bb7
vuillaut <thomas.vuillaume@gmail.com>
2019-06-12 14:31:34 +0200

    adding pyhessio required by ctapipe

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='dl1_data_handler',
       install_requires=[
           'numpy>=1.15.0',
           'tables>=3.4.4',
+          'pyhessio @ https://api.github.com/repos/cta-observatory/pyhessio/tarball/v2.1.1',
           'ctapipe @ https://api.github.com/repos/cta-observatory/ctapipe/tarball/v0.6.2',
           'eventio>=0.16.1',
           'ctapipe-extra @ https://api.github.com/repos/cta-observatory/ctapipe-extra/tarball/v0.2.17',],
