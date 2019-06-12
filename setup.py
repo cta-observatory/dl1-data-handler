8eadb7ba575da770f29ad9d81777971654f76d39
vuillaut <thomas.vuillaume@gmail.com>
2019-06-12 08:45:33 +0200

    tryiing with git+

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(name='dl1_data_handler',
       install_requires=[
           'numpy>=1.15.0',
           'tables>=3.4.4',
-          'ctapipe>=0.6.2',
+          'ctapipe=0.6.2',
           'eventio>=0.16.1',
           'ctapipe-extra>=0.2.17'],
       dependency_links=[
-      'https://github.com/cta-observatory/ctapipe/tarball/v0.6.2'
+      'git+https://github.com/cta-observatory/ctapipe.git'
       ],
       zip_safe=True)
