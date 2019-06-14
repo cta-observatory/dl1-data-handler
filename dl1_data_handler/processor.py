8b027b4cf0466a31f809d973d7efb446818d1bc6
mikael <mikael.jacquemont@lapp.in2p3.fr>
2019-06-14 16:12:05 +0200

    Add Transforms

--- dl1_data_handler/processor.py
+++ dl1_data_handler/processor.py
@@ -82,6 +82,7 @@ class ConvertShowerPrimaryIDToClassLabel(Transform):
                 example[i] = class_label
         return example
 
+
 class NormalizeTelescopePositions(Transform):
 
     def __init__(self, norm_x=1.0, norm_y=1.0, norm_z=1.0):
@@ -94,3 +95,117 @@ class NormalizeTelescopePositions(Transform):
                 normed_pos = arr / self.norms[des['base_name']]
                 example[i] = np.array(normed_pos, dtype=des['dtype'])
         return example
+
+
+class EnergyToLog(Transform):
+
+    def describe(self, description):
+        self.description = description
+        for des in self.description:
+            if des['base_name'] == 'mc_energy':
+                des['unit'] = 'log(TeV)'
+
+    def __call__(self, example):
+        for i, (val, des) in enumerate(zip(example, self.description)):
+            if des['base_name'] == 'mc_energy':
+                example[i] = np.log10(val)
+        return example
+
+
+class ImpactToKm(Transform):
+
+    def describe(self, description):
+        self.description = description
+        for des in self.description:
+            if des['base_name'] in ['core_x', 'core_y']:
+                des['unit'] = 'km'
+
+    def __call__(self, example):
+        for i, (val, des) in enumerate(zip(example, self.description)):
+            if des['base_name'] in ['core_x', 'core_y']:
+                example[i] = val / 1000
+        return example
+
+
+class XmaxToKm(Transform):
+
+    def describe(self, description):
+        self.description = description
+        for des in self.description:
+            if des['base_name'] == 'x_max':
+                des['unit'] = 'km'
+
+    def __call__(self, example):
+        for i, (val, des) in enumerate(zip(example, self.description)):
+            if des['base_name'] == 'x_max':
+                example[i] = val / 1000
+        return example
+
+
+class HfirstIntToKm(Transform):
+
+    def describe(self, description):
+        self.description = description
+        for des in self.description:
+            if des['base_name'] == 'h_first_int':
+                des['unit'] = 'km'
+
+    def __call__(self, example):
+        for i, (val, des) in enumerate(zip(example, self.description)):
+            if des['base_name'] == 'h_first_int':
+                example[i] = val / 1000
+        return example
+
+
+class TelescopePositionToKm(Transform):
+
+    def describe(self, description):
+        self.description = description
+        for des in self.description:
+            if des['base_name'] in ['x', 'y', 'z']:
+                des['unit'] = 'km'
+
+    def __call__(self, example):
+        for i, (val, des) in enumerate(zip(example, self.description)):
+            if des['base_name'] in ['x', 'y', 'z']:
+                example[i] = val / 1000
+        return example
+
+
+class DataForGammaLearn(Transform):
+
+    def __init__(self):
+        super().__init__()
+        self.mc_infos = ['mc_energy', 'core_x', 'core_y', 'alt', 'az', 'shower_primary_id', 'x_max', 'h_first_int']
+        self.array_infos = ['x', 'y', 'z']
+
+    def describe(self, description):
+        self.description = description
+        label_description = []
+        for des in self.description:
+            if des['base_name'] in self.mc_infos:
+                label_description.append(des['base_name'])
+        if len(label_description) > 0:
+            self.description.append({'name': 'label',
+                                     'tel_type': 'NA',
+                                     'base_name': 'label',
+                                     'shape': 'NA',
+                                     'dtype': 'NA',
+                                     'label_description': label_description})
+        return self.description
+
+    def __call__(self, example):
+        image = None
+        mc_energy = None
+        labels = []
+        array = []
+        for i, (val, des) in enumerate(zip(example, self.description)):
+            if des['base_name'] == 'image':
+                image = val
+            elif des['base_name'] in self.mc_infos:
+                labels.append(val)
+                if des['base_name'] == 'mc_energy':
+                    mc_energy = val
+            elif des['base_name'] in self.array_infos:
+                array.append(val)
+        return {'image': image, 'label': np.stack(labels), 'mc_energy': mc_energy, 'telescope': np.stack(array)}
