94787da99d6f987d9e136943bebfca273b622de9
nieto@talos <nieto@gae.ucm.es>
2019-07-06 18:40:25 +0200

    Adding event multiplicity filter

--- dl1_data_handler/utils.py
+++ dl1_data_handler/utils.py
@@ -33,6 +33,31 @@ def event_intensity_filter(reader, file, i_min=-np.inf, i_max=np.inf):
     mask2 = total_intensity < i_max
     return set(np.arange(len(file.root.Event_Info))[mask1 & mask2])
 
+def event_multiplicity_filter(reader, file, m_min=-np.inf, m_max=np.inf):
+    """
+    Prototype of Filter events on telescope multiplicity
+    Parameters
+    ----------
+    reader (DL1DataReader) : the reader to filter
+    file
+    m_min
+    m_max
+
+    Returns
+    -------
+    the filtered indices
+    """
+    # FIXME This implementation only works for mono or stereo reader mode, otherwise it only
+    # mask based on the last telescope type
+    tel_types = [reader.tel_type] if reader.mode in ['mono', 'stereo'] else list(reader.selected_telescopes)
+    multiplicity = np.zeros(len(file.root.Event_Info))
+    for tel_type in tel_types:
+        indices = file.root.Event_Info[:][tel_type + '_indices']
+        multiplicity = np.count_nonzero(indices,axis=1)
+    mask1 = m_min <= multiplicity
+    mask2 = multiplicity <= m_max
+    return set(np.arange(len(file.root.Event_Info))[mask1 & mask2])
+
 #################
 # Image Filters #
 #################
