0271aed9be6cc2d30931849e3e8d121dda1eccae
rcervinoucm <rcervino@ucm.es>
2024-06-06 12:51:56 +0200

    Update version.py

--- dl1_data_handler/version.py
+++ dl1_data_handler/version.py
@@ -84,6 +84,7 @@ def get_git_describe_version(abbrev=0):
         with open(devnull, "w") as fnull:
             
             arguments = [GIT_COMMAND, "describe", "--tags", "--always", "--abbrev=%d" % abbrev]
+            print('HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
             print(check_output(arguments, cwd=CURRENT_DIRECTORY, stderr=fnull).decode("ascii").strip())
             return (
                 check_output(arguments, cwd=CURRENT_DIRECTORY, stderr=fnull)
