0142b72434382ecceb86a33c4911f3fa232d9f23
rcervinoucm <rcervino@ucm.es>
2024-06-10 16:59:35 +0200

    Update exectestversion.py

--- exectestversion.py
+++ exectestversion.py
@@ -5,5 +5,5 @@ aux = get_version(pep440=False)
 print('aux')
 print(aux)
 details = {'version': aux}  
-with open('testversion.py', 'w') as convert_file: 
+with open('dl1_data_handler/testversion.py', 'w') as convert_file: 
      convert_file.write(json.dumps(details))
