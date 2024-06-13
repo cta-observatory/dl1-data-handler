d7ba6f8e5f415f95a4077472a6da3511d25da2e3
rcervinoucm <rcervino@ucm.es>
2024-06-13 12:27:31 +0200

    Update versionScript.py

--- .github/versionScript.py
+++ .github/versionScript.py
@@ -7,11 +7,12 @@ pprint.pprint(sys.path)
 # append the path of the
 # parent directory
 sys.path.append("..")
+pprint.pprint(sys.path)
 
-from dl1_data_handler import get_version_pypi
+#from dl1_data_handler import get_version_pypi
 
 def get_version():
-    return get_version_pypi()
+    return 0#get_version_pypi()
 
 
 if __name__ == "__main__":
