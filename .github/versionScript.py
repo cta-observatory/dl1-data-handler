528471b0817c9542f154a14e4825db9979b195a4
rcervinoucm <rcervino@ucm.es>
2024-06-13 11:56:34 +0200

    Update versionScript.py

--- .github/versionScript.py
+++ .github/versionScript.py
@@ -1,7 +1,13 @@
-import os
 import sys
-parent_directory = os.path.abspath('..')
-sys.path.append(parent_directory)
+import os
+
+# Get the parent directory
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+print(parent_dir)
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
+# Use a function from my_module
 from dl1_data_handler.version import get_version_pypi
 
 def get_version():
