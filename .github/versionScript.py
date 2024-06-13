32fb134b02345a5dbe954548f53a54dc193122a3
rcervinoucm <rcervino@ucm.es>
2024-06-13 11:51:19 +0200

    Update versionScript.py

--- .github/versionScript.py
+++ .github/versionScript.py
@@ -2,7 +2,7 @@ import os
 import sys
 parent_directory = os.path.abspath('..')
 sys.path.append(parent_directory)
-from dl1_data_handler.dl1_data_handler.version import get_version_pypi
+from dl1_data_handler.version import get_version_pypi
 
 def get_version():
     return get_version_pypi()
