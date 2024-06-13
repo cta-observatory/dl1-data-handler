d7c96d6dfd820fdf186744c364d8830e59c36234
rcervinoucm <rcervino@ucm.es>
2024-06-13 12:41:42 +0200

    Update versionScript.py

--- .github/versionScript.py
+++ .github/versionScript.py
@@ -1,10 +1,8 @@
 import sys
-import os
-
-#sys.path.append("..")
+sys.path.append('//home/runner/work/dl1-data-handler/dl1-data-handler/dl1-data-handler')
 
 #from ..dl1_data_handler.dl1_data_handler import get_version_pypi
-from . import dl1_data_handler
+import version
 def get_version():
     return get_version_pypi()
 
