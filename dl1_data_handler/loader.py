86aed24cab20f3db8475ae4674c1b926170edc05
Tjark Miener <tjark.miener4692@googlemail.com>
2025-01-13 15:56:05 +0100

    added a new reader for feature vectors (DLFeatureVectorReader)

--- dl1_data_handler/loader.py
+++ dl1_data_handler/loader.py
@@ -2,8 +2,9 @@ import numpy as np
 import astropy.units as u
 import keras
 from keras.utils import Sequence, to_categorical
- 
+
 from dl1_data_handler.reader import ProcessType
+
 
 class DLDataLoader(Sequence):
     """
@@ -24,10 +25,6 @@ class DLDataLoader(Sequence):
         Size of the batch to load the data.
     random_seed : int, optional
         Whether to shuffle the data after each epoch with a provided random seed.
-    sort_by_intensity : bool, optional
-        Whether to sort the events based on the hillas intensity for stereo analysis.
-    stack_telescope_images : bool, optional
-        Whether to stack the telescope images for stereo analysis.
 
     Methods:
     --------
@@ -62,19 +59,21 @@ class DLDataLoader(Sequence):
         self.sort_by_intensity = sort_by_intensity
 
         # Set the input shape based on the mode of the DLDataReader
-        if self.DLDataReader.mode == "mono":
-            self.input_shape = self.DLDataReader.input_shape
-        elif self.DLDataReader.mode == "stereo":
-            self.input_shape = self.DLDataReader.input_shape[
-                list(self.DLDataReader.selected_telescopes)[0]
-            ]
-            # Reshape inputs into proper dimensions for the stereo analysis with stacked images
-            if self.stack_telescope_images:
-                self.input_shape = (
-                    self.input_shape[1],
-                    self.input_shape[2],
-                    self.input_shape[0] * self.input_shape[3],
-                )
+        if self.DLDataReader.__class__.__name__ != "DLFeatureVectorReader":
+            if self.DLDataReader.mode == "mono":
+                self.input_shape = self.DLDataReader.input_shape
+            elif self.DLDataReader.mode == "stereo":
+                self.input_shape = self.DLDataReader.input_shape[
+                    list(self.DLDataReader.selected_telescopes)[0]
+                ]
+                # Reshape inputs into proper dimensions
+                # for the stereo analysis with stacked images
+                if self.stack_telescope_images:
+                    self.input_shape = (
+                        self.input_shape[1],
+                        self.input_shape[2],
+                        self.input_shape[0] * self.input_shape[3],
+                    )
 
     def __len__(self):
         """
@@ -106,6 +105,35 @@ class DLDataLoader(Sequence):
         """
         Generates one batch of data.
 
+        This method is called to generate one batch of data based on the index provided. It
+        retrieves the data from the DLDataReader and sets up the labels based on the tasks
+        specified.
+
+        Parameters:
+        -----------
+        index : int
+            Index of the batch to generate.
+
+        Returns:
+        --------
+        tuple
+            A tuple containing the input data as features and the corresponding labels.
+        """
+        features, labels = None, None
+        if self.DLDataReader.mode == "mono":
+            features, labels = self._get_mono_item(index)
+        elif self.DLDataReader.mode == "stereo":
+            features, labels = self._get_stereo_item(index)
+        return features, labels
+
+    def _get_mono_item(self, index):
+        """
+        Generates one batch of monoscopic data.
+
+        This method is called to generate one batch of monoscopic data
+        based on the index provided. It retrieves the data from the DLDataReader and
+        sets up the labels based on the tasks specified.
+
         Parameters:
         -----------
         index : int
@@ -121,44 +149,73 @@ class DLDataLoader(Sequence):
             index * self.batch_size : (index + 1) * self.batch_size
         ]
         labels = {}
-        if self.DLDataReader.mode == "mono":
-            batch = self.DLDataReader.generate_mono_batch(batch_indices)
-            # Retrieve the telescope images and store in the features dictionary 
-            features = {"input": batch["features"].data}
-            if "type" in self.tasks:
-                labels["type"] = to_categorical(
+        batch = self.DLDataReader.generate_mono_batch(batch_indices)
+        # Retrieve the telescope images and store in the features dictionary
+        features = {"input": batch["features"].data}
+        if "type" in self.tasks:
+            labels["type"] = to_categorical(
+                batch["true_shower_primary_class"].data,
+                num_classes=2,
+            )
+            # Temp fix till keras support class weights for multiple outputs or I wrote custom loss
+            # https://github.com/keras-team/keras/issues/11735
+            if len(self.tasks) == 1:
+                labels = to_categorical(
                     batch["true_shower_primary_class"].data,
                     num_classes=2,
                 )
-                # Temp fix till keras support class weights for multiple outputs or I wrote custom loss
-                # https://github.com/keras-team/keras/issues/11735
-                if len(self.tasks) == 1:
-                    labels = to_categorical(
-                        batch["true_shower_primary_class"].data,
-                        num_classes=2,
-                    )
-            if "energy" in self.tasks:
-                labels["energy"] = batch["log_true_energy"].data
-            if "direction" in self.tasks:
-                labels["direction"] = np.stack(
-                    (
-                        batch["spherical_offset_az"].data,
-                        batch["spherical_offset_alt"].data,
-                        batch["angular_separation"].data,
-                    ),
-                    axis=1,
-                )
-        elif self.DLDataReader.mode == "stereo":
-            batch = self.DLDataReader.generate_stereo_batch(batch_indices)
-            if self.DLDataReader.process_type == ProcessType.Simulation:
-                batch_grouped = batch.group_by(["obs_id", "event_id", "tel_type_id", "true_shower_primary_class"])
-            elif self.DLDataReader.process_type == ProcessType.Observation:
-                batch_grouped = batch.group_by(["obs_id", "event_id", "tel_type_id"])
-            features = []
-            true_shower_primary_class = []
-            log_true_energy = []
-            spherical_offset_az, spherical_offset_alt, angular_separation = [], [], []
-            for group_element in batch_grouped.groups:
+        if "energy" in self.tasks:
+            labels["energy"] = batch["log_true_energy"].data
+        if "direction" in self.tasks:
+            labels["direction"] = np.stack(
+                (
+                    batch["spherical_offset_az"].data,
+                    batch["spherical_offset_alt"].data,
+                    batch["angular_separation"].data,
+                ),
+                axis=1,
+            )
+        # Temp fix for supporting keras2 & keras3
+        if int(keras.__version__.split(".")[0]) >= 3:
+            features = features["input"]
+        return features, labels
+
+    def _get_stereo_item(self, index):
+        """
+        Generates one batch of stereoscopic data.
+
+        This method is called to generate one batch of stereoscopic data
+        based on the index provided. It retrieves the data from the DLDataReader and
+        sets up the labels based on the tasks.
+
+        Parameters:
+        -----------
+        index : int
+            Index of the batch to generate.
+
+        Returns:
+        --------
+        tuple
+            A tuple containing the input data as features and the corresponding labels.
+        """
+        # Generate indices of the batch
+        batch_indices = self.indices[
+            index * self.batch_size : (index + 1) * self.batch_size
+        ]
+        labels = {}
+        batch = self.DLDataReader.generate_stereo_batch(batch_indices)
+        if self.DLDataReader.process_type == ProcessType.Simulation:
+            batch_grouped = batch.group_by(
+                ["obs_id", "event_id", "tel_type_id", "true_shower_primary_class"]
+            )
+        elif self.DLDataReader.process_type == ProcessType.Observation:
+            batch_grouped = batch.group_by(["obs_id", "event_id", "tel_type_id"])
+        features, mono_feature_vectors, stereo_feature_vectors = [], [], []
+        true_shower_primary_class = []
+        log_true_energy = []
+        spherical_offset_az, spherical_offset_alt, angular_separation = [], [], []
+        for group_element in batch_grouped.groups:
+            if "features" in batch.colnames:
                 if self.sort_by_intensity:
                     # Sort images by the hillas intensity in a given batch if requested
                     group_element.sort(["hillas_intensity"], reverse=True)
@@ -168,10 +225,7 @@ class DLDataLoader(Sequence):
                     plain_features = group_element["features"].data
                     # Stack the telescope images along the last axis
                     stacked_features = np.concatenate(
-                        [
-                            plain_features[i]
-                            for i in range(plain_features.shape[0])
-                        ],
+                        [plain_features[i] for i in range(plain_features.shape[0])],
                         axis=-1,
                     )
                     # Append the stacked images to the features list
@@ -181,43 +235,61 @@ class DLDataLoader(Sequence):
                     # Append the plain images to the features list
                     # shape: (batch_size, n_tel, image_shape, image_shape, n_channels)
                     features.append(group_element["features"].data)
-                # Retrieve the labels for the tasks
-                # FIXME: This won't work for divergent pointing directions
-                if "type" in self.tasks:
-                    true_shower_primary_class.append(group_element["true_shower_primary_class"].data[0])
-                if "energy" in self.tasks:
-                    log_true_energy.append(group_element["log_true_energy"].data[0])
-                if "direction" in self.tasks:
-                    spherical_offset_az.append(group_element["spherical_offset_az"].data[0])
-                    spherical_offset_alt.append(group_element["spherical_offset_alt"].data[0])
-                    angular_separation.append(group_element["angular_separation"].data[0])
-            # Store the labels in the labels dictionary
+            # Retrieve the feature vectors
+            if "mono_feature_vectors" in batch.colnames:
+                mono_feature_vectors.append(group_element["mono_feature_vectors"].data)
+            if "stereo_feature_vectors" in batch.colnames:
+                stereo_feature_vectors.append(
+                    group_element["stereo_feature_vectors"].data
+                )
+            # Retrieve the labels for the tasks
+            # FIXME: This won't work for divergent pointing directions
             if "type" in self.tasks:
-                labels["type"] = to_categorical(
+                true_shower_primary_class.append(
+                    group_element["true_shower_primary_class"].data[0]
+                )
+            if "energy" in self.tasks:
+                log_true_energy.append(group_element["log_true_energy"].data[0])
+            if "direction" in self.tasks:
+                spherical_offset_az.append(group_element["spherical_offset_az"].data[0])
+                spherical_offset_alt.append(
+                    group_element["spherical_offset_alt"].data[0]
+                )
+                angular_separation.append(group_element["angular_separation"].data[0])
+        # Store the labels in the labels dictionary
+        if "type" in self.tasks:
+            labels["type"] = to_categorical(
+                np.array(true_shower_primary_class),
+                num_classes=2,
+            )
+            # Temp fix till keras support class weights for multiple outputs or I wrote custom loss
+            # https://github.com/keras-team/keras/issues/11735
+            if len(self.tasks) == 1:
+                labels = to_categorical(
                     np.array(true_shower_primary_class),
                     num_classes=2,
                 )
-                # Temp fix till keras support class weights for multiple outputs or I wrote custom loss
-                # https://github.com/keras-team/keras/issues/11735
-                if len(self.tasks) == 1:
-                    labels = to_categorical(
-                        np.array(true_shower_primary_class),
-                        num_classes=2,
-                    )
-            if "energy" in self.tasks:
-                labels["energy"] = np.array(log_true_energy)
-            if "direction" in self.tasks:
-                labels["direction"] = np.stack(
-                    (
-                        np.array(spherical_offset_az),
-                        np.array(spherical_offset_alt),
-                        np.array(angular_separation),
-                    ),
-                    axis=1,
-                )
-            # Store the fatures in the features dictionary 
+        if "energy" in self.tasks:
+            labels["energy"] = np.array(log_true_energy)
+        if "direction" in self.tasks:
+            labels["direction"] = np.stack(
+                (
+                    np.array(spherical_offset_az),
+                    np.array(spherical_offset_alt),
+                    np.array(angular_separation),
+                ),
+                axis=1,
+            )
+        # Store the fatures in the features dictionary
+        if "features" in batch.colnames:
             features = {"input": np.array(features)}
+        # TDOO: Add support for both feature vectors
+        if "mono_feature_vectors" in batch.colnames:
+            features = {"input": np.array(mono_feature_vectors)}
+        if "stereo_feature_vectors" in batch.colnames:
+            features = {"input": np.array(stereo_feature_vectors)}
         # Temp fix for supporting keras2 & keras3
         if int(keras.__version__.split(".")[0]) >= 3:
             features = features["input"]
+
         return features, labels
