f8964bfe210e112de2e0af2feaa8304404aa1f17
Tjark Miener <tjark.miener4692@googlemail.com>
2025-02-10 12:10:16 +0100

    add the r1 waveform reading unit test

--- dl1_data_handler/tests/test_reader.py
+++ dl1_data_handler/tests/test_reader.py
@@ -1,7 +1,7 @@
 import pytest
 from traitlets.config.loader import Config
 
-from dl1_data_handler.reader import DLImageReader
+from dl1_data_handler.reader import DLImageReader, DLWaveformReader
 
 def test_dl1_image_reading(dl1_tmp_path, dl1_gamma_file):
     """check reading from pixel-wise image data files"""
@@ -21,3 +21,24 @@ def test_dl1_image_reading(dl1_tmp_path, dl1_gamma_file):
     mono_batch = dl1_reader.generate_mono_batch([0])
     assert mono_batch["tel_id"] == 4
     assert mono_batch["features"].shape == (1,  110, 110, 2)
+
+
+def test_r1_waveform_reading(r1_tmp_path, r1_gamma_file):
+    """check reading from pixel-wise waveform data files"""
+    # Create a configuration suitable for the test
+    config = Config(
+        {
+            "DLWaveformReader": {
+                "allowed_tels": [4],
+                "sequence_length": 20,
+            },
+        }
+    )
+    # Create an image reader and test basic properties
+    r1_reader = DLWaveformReader(input_url_signal=[r1_gamma_file], config=config)
+    assert r1_reader._get_n_events() == 1
+    assert r1_reader.tel_type == "LST_LST_LSTCam"
+    # Test the generation of a mono batch
+    mono_batch = r1_reader.generate_mono_batch([0])
+    assert mono_batch["tel_id"] == 4
+    assert mono_batch["features"].shape == (1,  110, 110, 20)
